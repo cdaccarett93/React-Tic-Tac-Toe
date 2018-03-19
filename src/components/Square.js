b879fc67d75528e987215687c1fc0f71e00a06d8
Carlos Daccarett <cdaccarett93@gmail.com>
2018-03-18 20:35:13 -0700

    basic functionality and winner calculation added

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Square extends Component {
-  render() {
-    return <button className="square">{/* TODO */}</button>;
-  }
+export default function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
-
-export default Square;
